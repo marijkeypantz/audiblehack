618ca983455c21d4a2fac33262dd809b6a506d92
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-04-16 11:13:56 -0400

    Updated functions to get the shortest quote.

--- sketch.js
+++ sketch.js
@@ -1,56 +1,57 @@
-
-
-var lines = [];//we declare lines as an empty array
+var lines = []; //we declare lines as an empty array
 
 //preload gets called BEFORE setup
-function preload(){
+function preload() {
   //loadStrings returns an array of Strings from our text file
   //you must have a local file in order for this function to work
   lines = loadStrings('assets/thebelljar.txt');
-
+  // SETUP PRELOAD ENDS
 }
 
 function setup() {
-  createCanvas(800,800);
+  createCanvas(800, 800);
   textAlign(LEFT, TOP);
   //joins an array into a long string
   //and puts empty space between array elements
   var linesAsString = lines.join(' ');
-  
+
   //in order to split by sentence,
   //we need to have our lines as a single long string
   var sentences = RiTa.splitSentences(linesAsString);
-  
-  text('there are ' + sentences.length + ' sentences in this book', 0,10);
-  
-  // var blah = sentences[500];
-  //text(sentences[2500],0,30);
-  //loop through each of the sentences
-  //and do something with each of the sentences
-  x =1;
-  var results = ["NULL"];
-  var resLength = [0];
-  for(var i=0; i < sentences.length;i++){
+
+  text('there are ' + sentences.length + ' sentences in this book', 0, 10);
+
+  x = 1;
+  var results = [];
+  var resLength = [];
+  // FOR LOOP THAT LOOKS FOR SEARCH TERM IN EACH SENTENCE AND SAVES MATCHES TO NEW ARRAY
+  for (var i = 0; i < sentences.length; i++) {
     //your code here
-    var n = sentences[i].search("Coney Island");
-    
-    if (n!= -1) {
+    var n = sentences[i].search("Madison Avenue");
+      // text(n,0,100+(10*i));
+      if(n != -1) {
+      results.push(sentences[i]);
+      resLength.push(sentences[i].length);
+      // text(sentences[i],0,35+(15*x),800,800);
+      x = x + 5;
+      }
       
-    
-    results.push(sentences[i]);
-    resLength.push(sentences[i].length);
-    x = x+1;
-    }
-    
-  }
-  maximum = resLength.max();
-  for(var i=1; i < results.length;i++){
-  
-    if (resLength[i] == maximum) {
-      text(sentences[i],10,30+(35*x),800,800);
-    }
-    
-    
   }
   
+
+  minimum = getMinOfArray(resLength);
+  // text(minimum, 0, 50);
+  for (var i = 0; i < results.length; i++) {
+    // text(resLength[i],0,50+(10*i));
+    if (resLength[i] == minimum) {
+      text(results[i],10,30+(35*x),800,800);
+    }
+
+  }
+  //  SETUP FUNCTION ENDS
+}
+
+
+function getMinOfArray(numArray) {
+  return Math.min.apply(null, numArray);
 }
